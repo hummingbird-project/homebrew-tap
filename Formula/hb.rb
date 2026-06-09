616c69aad05365d493ccd843dc00df2a02f130bc
Adam Fowler <adamfowler71@gmail.com>
2026-06-09 17:25:33 +0000

    hb: add 0.1.2 bottle.

--- Formula/hb.rb
+++ Formula/hb.rb
@@ -5,6 +5,12 @@ class Hb < Formula
   sha256 "f97be5b7d7a1c08e54a4e09c492fe2bcda7421ead537493a7cbe00d0b6179cc3"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/hummingbird-project/homebrew-tap/releases/download/hb-0.1.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "2e2e7b9a37abbc658db834d4c16a55dea0be4eed2cb97008eae1ac25349b5f94"
+    sha256 cellar: :any_skip_relocation, tahoe:       "b1ead328766437ae060780acd981dc8d19a68ba4f203a90bab5cd7b79c547550"
+  end
+
   depends_on xcode: ["26.2", :build]
   uses_from_macos "swift" => :build
 
